a08cb844683e346218c8ce970ede83e2c9b28509
Shishkin PRO <viktorshish@gmail.com>
2024-03-08 12:39:11 +0300

    Normalized the phone numbers

--- property/admin.py
+++ property/admin.py
@@ -6,10 +6,19 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_display = [
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+        'owners_phonenumber',
+        'owner_pure_phone'
+    ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['like']
+
 
 
 class ComplaintAdmin(admin.ModelAdmin):
