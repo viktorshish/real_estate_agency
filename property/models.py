07da6a59578ffa35eafe0e926abcfac32f823f8c
Shishkin PRO <viktorshish@gmail.com>
2024-03-10 20:40:56 +0300

    Delete owner from model Flat

--- property/models.py
+++ property/models.py
@@ -5,8 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
-
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
