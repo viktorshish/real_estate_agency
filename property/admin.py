443cff47cb923fdf89ea2eb4fae332fe2bfdaf6d
Shishkin PRO <viktorshish@gmail.com>
2024-03-10 22:00:53 +0300

    Showed the owners in the Flat

--- property/admin.py
+++ property/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ['owner']
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
     list_display = [
         'address',
@@ -16,6 +21,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['like']
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
