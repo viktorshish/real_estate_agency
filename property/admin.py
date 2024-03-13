45df263d7592ee8f3b7a59f8b822aec08b2a37b0
Viktor Shishkalov <viktorshish@gmail.com>
2024-03-13 12:05:20 +0300

    Apply @admin

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInline(admin.TabularInline):
     raw_id_fields = ['owner']
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
@@ -24,16 +25,13 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerInline]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ("flat",)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['name', 'pure_phone']
     search_fields = ['name']
     raw_id_fields = ("flats",)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
